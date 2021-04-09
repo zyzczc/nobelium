9914ccac9573b650dcfc7136f16d487b606b8603
zyzczc <59289540+zyzczc@users.noreply.github.com>
2021-04-09 23:08:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Let Go',
+  author: 'Le go',
+  email: '875515536@qq.com',
+  link: 'https://zyzczc.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
   appearance: 'auto', // ['light', 'dark', 'auto'],
